b515a4aafb14ba1970318543e54d6494ba010486
Unknown <rafaestaenclase@gmail.com>
2019-02-26 13:56:57 +0100

    login

---
+++ controller/AdministradorController.php
@@ -0,0 +1,136 @@
+<?php
+
+    session_start();
+    require_once __DIR__."/../controller/GeneralController.php";
+    class AdministradorController extends GeneralController {
+        private $connect;
+        private $connection;
+
+        public function __construct(){
+            require_once __DIR__."/../core/Connection.php";
+            require_once __DIR__."/../model/Administrador.php";
+
+
+            $this->connect = new Connection();
+            $this->connection = $this->connect->conexion();
+        }
+
+        function runLogin(){
+			$parameters = Array();
+			$this->view('login', $parameters);
+		}
+
+		function verifyLogin(){
+			if(isset($_POST["usuario"], $_POST["contrasenna"])){
+                $admin = new Administrador($this->connection);
+                $dbuser = $admin->getHashByUsuarioContrasenna($_POST["usuario"], $_POST["contrasenna"]);
+                if($dbuser == null){
+                    echo "ERRO DE INICIO";
+                    $parameters = Array();
+					$this->view('login', $parameters);
+                }else{
+                    $_SESSION["hash"] = $dbuser[0]["hash"];
+                    $parameters = Array();
+					$this->view('index', $parameters);
+                }
+            }else{
+            	$parameters = Array();
+				$this->view('login', $parameters);
+            }
+			
+		}
+
+		function logOut(){
+			session_destroy();
+			$parameters = Array();
+			$this->view('index', $parameters);
+		}
+
+		function getAll(){
+			$connection = new Connection();
+			$post = new Post($connection);
+			$resul = $post->getAll();
+			return $resul;
+		}
+
+		function getPostByProfile(){
+			$connection = new Connection();
+			$post = new Post($connection);
+			$publications = $post->getPostByProfile($_SESSION["profile"]["id"]);
+			
+			return $publications;
+		}
+
+		function getPostById(){
+			$connection = new Connection();
+			$post = new Post($connection);
+			$publications = $post->getPostById($_GET["idPost"]);
+			return $publications;
+		}
+
+		function getPostByNickname(){
+			$connection = new Connection();
+			$post = new Post($connection);
+			$publications = $post->getPostByNickname($_GET["nickname"]);
+			return $publications;
+		}
+
+		function getPostByCategory(){
+			$post = new Post($this->connection);
+			$publications = $post->getPostByCategory($_GET["nameCategory"]);
+			$this->view('search', array("publications" => $publications));
+		}
+
+		function getPostByTitle(){
+			$connection = new Connection();
+			$post = new Post($connection);
+			$publications = $post->getPostByTitle($_GET["value"]);
+			return $publications;
+		}
+
+
+		function getFollowedPosts(){
+			$post = new Post($this->connection);
+			$resul = $post->getFollowedPosts($_SESSION["profile"]["id"]);
+			return $resul;
+		}
+
+		function getRefreshPosts(){
+			$post = new Post($this->connection);
+			$resul = $post->getRefreshPosts($_SESSION["profile"]["id"], $_GET["date"]);
+			echo $resul;
+		}
+
+		function insertPost(){
+			$post = new Post($this->connection);
+			$category = new Category($this->connection);
+			$resul = $category->getCategoryByName($_POST["category"]);
+
+			if ($resul == null) {
+				$category->insertCategory($_POST["category"]);
+				$resul = $category->getCategoryByName($_POST["category"]);
+			}
+
+			$idP = $post->insertPost($_SESSION["profile"]["id"], $_POST["title"], $_POST["rawData"], date("d-m-Y H:i:s"), $resul[0]["id"]);
+			
+			$archiveTem = $_FILES['archive']['tmp_name'];
+			$name = $_FILES['archive']['name'];
+			$type = $_FILES['archive']['type'];
+			$size = $_FILES['archive']['size'];
+
+			$destiny = "img/postImg/".$idP."_".$name;
+
+			if (move_uploaded_file($archiveTem, $destiny)) {
+				$archive = new Archive($this->connection);
+				$idP = $archive->insertArchive($destiny, $idP);
+			}
+			header("location:index.php");
+		}
+
+
+		
+
+	}
+
+
+?>
--- controller/GeneralController.php
+++ controller/GeneralController.php
@@ -11,6 +11,13 @@ class GeneralController{
     }
 
     public function view($view, $data){
+        if(!isset($_SESSION)) 
+        { 
+            session_start(); 
+        } 
+        if (isset($_SESSION["hash"])) {
+            $data["hash"] =  $_SESSION["hash"];
+        }
         require_once __DIR__."/../vendor/autoload.php";
         $loader = new Twig_Loader_Filesystem(__DIR__."/../view");
         $twig = new Twig_Environment($loader);
